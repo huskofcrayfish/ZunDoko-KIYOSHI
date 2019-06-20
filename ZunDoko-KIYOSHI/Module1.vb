2b4a1ac2b19806397f35ff92809b155ca9f07a8d
Nao <Na0Halmi@gmail.com>
2019-06-21 00:45:06 +0900

    initial commit

---
+++ ZunDoko-KIYOSHI/Module1.vb
@@ -0,0 +1,16 @@
+﻿Module Module1
+    Sub Main()
+        Dim z, i As Byte
+        Dim rnd = New System.Random
+        Do Until (z = 30)
+            i = rnd.Next(4) \ 2
+            If i = 1 Then
+                Console.WriteLine("ズン")
+            Else
+                Console.WriteLine("ドコ")
+            End If
+            z = ((z << 4) >> 3) Or i
+        Loop
+        Console.WriteLine("キ・ヨ・シ！")
+    End Sub
+End Module
